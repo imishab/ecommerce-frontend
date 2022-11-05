ebd70983f093f62d6e8397dc210b205dc26b84e8
Mishab <85479103+imishab@users.noreply.github.com>
2022-11-05 14:25:42 +0000

    Update urlConfig.js

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,4 +1,4 @@
-const baseUrl = "https://flipkart-rest-server.herokuapp.com";
+const baseUrl = "https://forskor-rest-server.herokuapp.com";
 
 export const api = `${baseUrl}/api`;
 export const generatePublicUrl = (fileName) => {
